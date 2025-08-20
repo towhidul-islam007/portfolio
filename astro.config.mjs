36a2b10f57a550c87ca154464d277cf92c02a437
Towhidul Islam <im.towhidul@gmail.com>
2025-08-20 21:47:00 +0600

    feat: Add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // Uncomment and modify if deploying to a subdirectory
+  // site: 'https://yourdomain.com',
+  // base: '/subdirectory',
 });
